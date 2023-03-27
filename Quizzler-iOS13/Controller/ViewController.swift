22eb9b5c396fa3a5cc07782175eec9a9f0a9f2eb
Thiago Pereira <thiagorpereira7@gmail.com>
2023-03-27 14:12:47 -0300

    feat: add quiz project with README

---
+++ Quizzler-iOS13/Controller/ViewController.swift
@@ -0,0 +1,64 @@
+//
+//  ViewController.swift
+//  Quizzler-iOS13
+//
+//  Created by Angela Yu on 12/07/2019.
+//  Copyright © 2019 The App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var scoreLabel: UILabel!
+    @IBOutlet weak var questionLabel: UILabel!
+    @IBOutlet weak var progressBar: UIProgressView!
+    
+    @IBOutlet weak var firstButton: UIButton!
+    @IBOutlet weak var secondButton: UIButton!
+    @IBOutlet weak var thirdButton: UIButton!
+    
+    
+    var quizBrain = QuizBrain()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        updateUI()
+    }
+
+    @IBAction func answerButtonPressed(_ sender: UIButton) {
+        
+        let userAnswer = sender.currentTitle
+        let userGotItRight = quizBrain.checkAnswer(userAnswer!)
+        
+        if userGotItRight {
+            sender.backgroundColor = UIColor.green
+        } else {
+            sender.backgroundColor = UIColor.red
+        }
+
+        quizBrain.nextQuestion()
+        
+        
+        Timer.scheduledTimer(timeInterval: 0.2, target: self, selector: #selector(updateUI), userInfo: nil, repeats: false)
+        
+    }
+    
+    @objc func updateUI() {
+        scoreLabel.text = "Score: \(quizBrain.getScore())"
+        questionLabel.text = quizBrain.getQuestionText()
+        
+        firstButton.backgroundColor = UIColor.clear
+        secondButton.backgroundColor = UIColor.clear
+        thirdButton.backgroundColor = UIColor.clear
+        
+        let answers = quizBrain.getAnswersText()
+        firstButton.setTitle(answers[0], for: .normal)
+        secondButton.setTitle(answers[1], for: .normal)
+        thirdButton.setTitle(answers[2], for: .normal)
+        
+        progressBar.progress = quizBrain.getProgress()
+    }
+    
+}
+
---
+++ README.md
@@ -0,0 +1,28 @@
+#  Quizzler
+
+
+<h1 align="center" display="flex">
+        Quizzler
+     <img alt="D" title="DevRadar" src="Documentation/iconApp.png" width="80px" />
+</h1>
+
+<h1 align="center">
+  Quiz App Multiple Choice
+</h1>
+
+<h1 align="center">
+     <img alt="D" title="DevRadar" src="Documentation/app.gif" width="250px" />
+</h1>
+
+<h1 align="center">
+  App ultilazando o padrão MVC
+</h1>
+
+* What is a design pattern and how is it used in programming.
+* How to use the Model-View-Controller or MVC pattern for app development.
+* Swift structures and functions with outputs.
+* Learn about Object Oriented Programming.
+* Learn to use UIAlertControllers to display popups to the user.
+* How to refactor code and stay organised.
+* Learn about object initialisation. 
+* How to programmatically change UI elements such as Labels and Views.
